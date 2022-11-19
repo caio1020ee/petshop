902371b2600b2d9adbd7b798c88cda2accdaf762
marcossiuves <marcossiuves@gmail.com>
2022-11-19 15:19:02 -0300

    initial commit

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,35 @@
+import 'package:shop/models/product.dart';
+
+final dummyProducts = [
+  Product(
+    id: 'p1',
+    name: 'Husky siberiano',
+    description: 'A red shirt - it is pretty red!',
+    price: 2,
+    imageUrl:
+        'https://t2.uc.ltmcdn.com/pt/posts/6/6/9/como_fazer_um_filhote_de_cachorro_parar_de_chorar_17966_orig.jpg',
+  ),
+  Product(
+    id: 'p2',
+    name: 'Labrador',
+    description: 'A nice pair of trousers.',
+    price: 5,
+    imageUrl:
+        'https://s2.glbimg.com/rcGtLi2fl05H_YxgDPKQuiE2O_4=/0x71:960x991/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2020/6/I/k8jYDQRJANtwOWgNp4uA/whatsapp-image-2020-08-25-at-15.07.29-1-.jpeg',
+  ),
+  Product(
+    id: 'p3',
+    name: 'Fila brasileiro',
+    description: 'Warm and cozy - exactly what you need for the winter.',
+    price: 3,
+    imageUrl: 'https://www.petz.com.br/cachorro/racas/fila-brasileiro/img/fila-brasileiro-filhote.webp',
+  ),
+  Product(
+    id: 'p4',
+    name: 'Pastor Alemão',
+    description: 'Prepare any meal you want.',
+    price: 1,
+    imageUrl:
+        'https://blog.cobasi.com.br/wp-content/webpc-passthru.php?src=https://blog.cobasi.com.br/wp-content/uploads/2020/11/pastor-alemao-filhote-pequeno.png&nocache=1',
+  ),
+];
---
+++ lib/utils/constants.dart
@@ -0,0 +1,10 @@
+class Constants {
+  static const userFavoritesUrl =
+      
+'https://pet-shop-11ec8-default-rtdb.firebaseio.com/userFavorites';
+  static const productBaseUrl =
+      'https://pet-shop-11ec8-default-rtdb.firebaseio.com/products';
+  static const orderBaseUrl =
+      'https://pet-shop-11ec8-default-rtdb.firebaseio.com/orders';
+  static const webApiKey = 'AIzaSyAFnLetYw5nC2M2LDCUVVFpF3drNLAsWVA';
+}
